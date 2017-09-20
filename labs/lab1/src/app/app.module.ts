3ca97476d7fd6a0c782d471b6163df565bc7c6a5
Chris CHIN <CKH246@corp.ha.org.hk>
2017-09-20 16:23:12 +0800

    Test

--- labs/lab1/src/app/app.module.ts
+++ labs/lab1/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './component/customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
