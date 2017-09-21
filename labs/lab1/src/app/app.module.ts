2308b51798ed91f1694c6788f969d5d00cb56ae2
Chris CHIN <CKH246@corp.ha.org.hk>
2017-09-21 10:08:27 +0800

    Added more components

--- labs/lab1/src/app/app.module.ts
+++ labs/lab1/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {CustomerComponent} from './component/customer/customer.component'
 import { MainLibraryModule } from './module/main-library/main-library.module';
+import { MasterPageComponent } from './Component/master-page/master-page.component';
+import { SupplierComponent } from './Component/supplier/supplier.component';
+import { WelcomeComponent } from './Component/welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    MasterPageComponent,
+    SupplierComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
---
+++ labs/lab1/src/app/component/master-page/master-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'main-ui',
+  templateUrl: './master-page.component.html',
+  styleUrls: ['./master-page.component.css']
+})
+export class MasterPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
